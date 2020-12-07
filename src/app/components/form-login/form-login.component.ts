46a7fea3d7b92248897c83e4ef78638945b91bc3
Julien Noyer <j.noyer@digitalworkshop.fr>
2020-12-07 16:48:52 +0100

    Ajout de la fonction resetForm

--- src/app/components/form-login/form-login.component.ts
+++ src/app/components/form-login/form-login.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+// Importer les module pour la gestion du formulaire
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html'
 })
 export class FormLoginComponent implements OnInit {
 
-  constructor() { }
+  // Déclarer une variable pour le formulaire
+  public formData: FormGroup;
+
+  // Injecter FormBuilder dans la classe
+  constructor(
+    private FormBuilder: FormBuilder
+  ) { }
+
+  // Créer une fonction pour initialiser le formulaire
+  private resetForm = function(){
+    // Définir la valeur initialle de formData
+    this.formData = this.FormBuilder.group({
+      email: [ null, Validators.required ],
+      password: [ null, Validators.required ]
+    });
+  };
 
   ngOnInit(): void {
   }
